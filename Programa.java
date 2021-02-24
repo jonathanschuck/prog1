d10330caf5bcbc148c7e6838815369497cb2f7fb
jonathanschuck <jonathanschuck1@gmail.com>
2021-02-24 20:21:03 -0300

    aula 1

---
+++ Programa.java
@@ -0,0 +1,12 @@
+public class Programa {
+    public static void main(String[] args) {
+        int inteiro = 30;
+        String nome = "Jonathan Schuck";
+
+        System.out.print("Nome: " + nome);
+
+        System.out.print("Idade: " + inteiro);
+        
+    }
+    
+}
