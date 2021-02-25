1414663731babd22d5a7fec0a503b87f9f48359c
jonathanschuck <jonathanschuck1@gmail.com>
2021-02-24 21:57:23 -0300

    java

---
+++ ExercicioUm.java
@@ -0,0 +1,11 @@
+public class ExercicioUm{
+    public static void main(String[] args){
+        System.out.print("\nProfessor: Fernando Bevilacqua ");
+        System.out.print("\nAluno: Seu nome aqui ");
+        System.out.print("\nObjetivo: ");
+        System.out.print("\n       O objetivo dessa disciplina é compreender os conceitos fundamentais \n       do paradigma de programação orientada a objetos e aplicá-los no \n       desenvolvimento de soluções de software.");
+        System.out.print("\nTurno: Vespertino ");
+    }
+
+
+}
--- Programa.java
+++ Programa.java
@@ -1,11 +1,44 @@
+import java.util.*;
+
 public class Programa {
     public static void main(String[] args) {
         int inteiro = 30;
         String nome = "Jonathan Schuck";
 
-        System.out.print("Nome: " + nome);
+        Scanner scanner;
 
-        System.out.print("Idade: " + inteiro);
+        scanner = new Scanner(System.in);
+
+        System.out.print("\nDigite a idade: ");
+        inteiro = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("\nDigite seu Nome: ");
+        nome = scanner.nextLine();
+
+        if (inteiro == 30) {
+            System.out.print("Voce tem 30 anos ***** ");
+        }else{
+            System.out.print("Voce não tem 30 anos ***** ");
+        }
+
+        for (int i = 0; i < 10; i++){
+
+            System.out.print("\nHello");
+        }
+
+        System.out.print("\nNome: " + nome);
+
+        System.out.print("\nIdade: " + inteiro);
+
+        switch (inteiro){
+            case 30: {
+                System.out.print("\nCASE");
+            }
+
+        
+
+        }
         
     }
     
